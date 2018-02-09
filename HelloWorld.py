8c9a473ad66a93dce089e07de000d759ee997c37
KaistInstitute <ml634@kaist.ac.kr>
2018-02-09 10:58:27 +0900

    adding helloworld file

---
+++ HelloWorld.py
@@ -0,0 +1,17 @@
+##############################################
+#
+#Purpose: Print Text & update using Git
+#Author: Moonyoung Lee (ml634@kaist.ac.kr)
+#Date:  02.2018
+#
+##############################################
+
+
+
+print "HelloWorld!"
+
+#print out today's time
+print "Moonyoung was here on 02.07.2018"
+
+#print out name and date
+#print "[] was here on []"
