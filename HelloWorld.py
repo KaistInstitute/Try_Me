64af3e4f1685f1822f541a8457c0fecb906b52c1
chosaihim <chosaihim@gmail.com>
2018-07-10 09:30:28 +0900

    added to HelloWorld tutorial

--- HelloWorld.py
+++ HelloWorld.py
@@ -16,4 +16,5 @@ print "Moonyoung was here on 	02.07.2018"
 #ADD HERE: print out name and date
 print "Lee was here on 			02.08.2018"
 print "Mark was here on			02.08.2018"
+print "SC was here on			07.10.2018"
 #print "[] was here on []"
